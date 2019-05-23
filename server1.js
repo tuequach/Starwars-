ca88bf8fc32dbb3c8f8028cc52b6e673c10ad85f
tuequach <tuequach@gmail.com>
2019-05-22 19:19:12 -0500

    acitivites 9

--- server1.js
+++ server1.js
@@ -3,36 +3,28 @@
 var express = require("express");
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = 3000;
+
 // Data
-// ===========================================================
-var yoda = {
+var characters = [{
+  routeName: "yoda",
   name: "Yoda",
   role: "Jedi Master",
   age: 900,
   forcePoints: 2000
-};
-
-var darthmaul = {
+}, {
+  routeName: "darthmaul",
   name: "Darth Maul",
   role: "Sith Lord",
   age: 200,
   forcePoints: 1200
-};
-
-var obiwankenobi = {
+}, {
+  routeName: "obiwankenobi",
   name: "Obi Wan Kenobi",
-  role: "Jedi",
-  age: 50,
-  forcePoints: 1000
-};
-// Create one more data entry for the character Obi Wan Kenobi.
-// Enter any values you like for the parameters following the same format as the Yoda and Darth Maul character
-//
-
-// YOUR CODE GOES HERE
-
-//
+  role: "Jedi Knight",
+  age: 60,
+  forcePoints: 1350
+}];
 
 // Routes
 // ===========================================================
@@ -40,25 +32,15 @@ app.get("/", function(req, res) {
   res.send("Welcome to the Star Wars Page!");
 });
 
-app.get("/yoda", function(req, res) {
-  res.json(yoda);
+app.get("/:character", function(req, res) {
+  var chosen = req.params.character;
+
+  // What does this log?
+  console.log(chosen);
+
+  res.end();
 });
 
-app.get("/darthmaul", function(req, res) {
-  res.json(darthmaul);
-});
-
-app.get('/obiwankenobi', function (req,res) {
-  res.json(obiwankenobi);
-});
-
-// Create a new Express route that leads users to the new Obi Wan Kenobi Data
-// Follow the same format as the Yoda and Darth Maul routes
-//
-
-// YOUR CODE GOES HERE
-//
-//
 
 // Listener
 // ===========================================================
